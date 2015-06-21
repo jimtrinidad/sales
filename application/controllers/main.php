c6d3e3839085b24b6f2fdc79b6a781add0e59735
jimtrinidad <jim.trinidad002@gmail.com>
2015-06-21 19:09:58 +0800

    upload last row empty resulting to error fix

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1657,7 +1657,7 @@ if($weeks[date('Y-m-d',strtotime ( "-{$i} week" , $date_to))] >= 300) {
 							$all_fields_empty = false;
 						}
 
-						$value = preg_replace('/[\x00-\x1f]/', '?', $value);
+						$value = utf8_encode($value);
 
 						$field = array(
 								'value'		=> $value,
