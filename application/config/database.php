0174b6890ef94c2184c31f88fdff064d7a41cad7
jimtrinidad <jim.trinidad002@gmail.com>
2015-06-23 19:43:33 +0800

    a

--- application/config/database.php
+++ application/config/database.php
@@ -43,8 +43,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'bsiserver';
-$db['default']['database'] = 'db_sales_live';
+$db['default']['password'] = '';
+$db['default']['database'] = 'salesv1';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
